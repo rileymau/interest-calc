5f3de0678f3a53adde008278dd44e61fb855c2ef
rileymau <morgannmistweaver@protonmail.com>
2025-08-11 18:56:18 -0500

    add files, validator

---
+++ InputValidator.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.InputMismatchException;
+
+public class InputValidator {
+    /**
+     * Validates that a number is positive
+     * Throws IllegalArgumentException if not positive
+     */
+    public static void validatePositive(double value, String fieldName) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(fielName + "must be greater than 0.");
+        }
+    }
+
+    /**
+     * Tries to parse a string into a double
+     * Throws NumberFormatException if parsing fails
+     */
+    public static double parseDouble(String input, String fieldName) {
+        try {
+            double value = Double.parseDouble(input);
+            validatePositive(value, fieldName);
+            return value;
+        } catch (NumberFormatException e) {
+            throw new InputMismatchException("Invalid input for " + fieldName + ". Pleae enter a number.")
+        }
+    }
+}
---
+++ InterestService.java
@@ -0,0 +1,1 @@
+-- Logic to calculate interest
